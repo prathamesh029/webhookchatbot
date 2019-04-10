323c5951de9c755b0d5fffdad0e1370144ffd9ee
prathamesh029 <38250203+prathamesh029@users.noreply.github.com>
2019-04-10 14:35:50 +0530

    mynode

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-'use strict';
- 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express().use(bodyParser.json()); // creates http server
-const token = 'VERIFICATION_TOKEN'; // type here your verification token
- 
-app.listen(3000, () => console.log('[ChatBot] Webhook is listening'));
-
-app.get('/', (req, res) => {
-    // check if verification token is correct
-    if (req.query.token !== token) {
-        return res.sendStatus(401);
-    }
- 
-    // return challenge
-    return res.end(req.query.challenge);
-});
-
-app.post('/', (req, res) => {
-    // check if verification token is correct
-    if (req.query.token !== token) {
-        return res.sendStatus(401);
-    }
- 
-    // print request body
-    console.log(req.body);
- 
-    // return a text response
-    const data = {
-        responses: [
-            {
-                type: 'text',
-                elements: ['Hi', 'Hello']
-            }
-        ]
-    };
- 
-    res.json(data);
-});
+// import express JS module into app 
+// and creates its variable. 
+var express = require('express'); 
+var app = express(); 
+  
+// Creates a server which runs on port 3000 and  
+// can be accessed through localhost:3000 
+app.listen(3000, function(){ 
+    console.log('server running on port 3000'); 
+} ) 
+  
+// Function callName() is executed whenever  
+// url is of the form localhost:3000/name 
+app.get('/name', callName); 
+  
+function callName(req, res) { 
+      
+    // Use child_process.spawn method from  
+    // child_process module and assign it 
+    // to variable spawn 
+    var spawn = require("child_process").spawn; 
+      
+    // Parameters passed in spawn - 
+    // 1. type_of_script 
+    // 2. list containing Path of the script 
+    //    and arguments for the script  
+      
+    // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
+    // so, first name = Mike and last name = Will 
+    var process = spawn('python',["./mypyttoncode.py", 
+                            req.query.firstname, 
+                            req.query.lastname,
+                            req.query.pp] ); 
+  
+    // Takes stdout data from script which executed 
+    // with arguments and send this data to res object 
+    process.stdout.on('data', function(data) { 
+        res.send(data.toString()); 
+    } ) 
+} 
